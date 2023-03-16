8510a1688033a743ca94f7eaa4bf2339a52222f5
noahnim <noahmedoff@gmail.com>
2023-03-16 11:23:17 -0700

    contains duplicate in array

---
+++ arrayContainsDuplicate.py
@@ -0,0 +1,13 @@
+# if a number appears twice return true
+# make a dictionary to put numbers as keys and values as counts
+# if the numnber appears more than once return true
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        numsCount = dict()
+        for number in nums:
+            if number in numsCount:
+                return True
+            else:
+                numsCount[number] = 1
+        return False
